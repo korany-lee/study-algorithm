28b9b64b5cffec059bf0fd798a1acee285869ad8
QuePark <gyuhapark11@gmail.com>
2020-09-02 13:32:45 +0900

    Complete: toyProblem 2 by gray

---
+++ codeSharing/toyProblem/02_nonrepeatCharacter/gray.js
@@ -0,0 +1,27 @@
+const firstNonRepeatedCharacter = function (string) {
+	// TODO: Your code here!
+	let tmpArr = string.split('');
+	let obj = {};
+	let result = [];
+
+	// 객체에 str의 모든 글자마다 카운팅
+	for (let key of tmpArr) {
+		if (key in obj === false) {
+			obj[key] = 0;
+		}
+		obj[key]++;
+	}
+
+	// 카운트가 1인 것만 걸러줌
+	for (let key in obj) {
+		if (obj[key] === 1) {
+			result.push(key);
+		}
+	}
+
+	// 카운트가 1인 친구들의 인덱스를 찾음
+	result = result.map((x) => tmpArr.indexOf(x));
+
+	// 인덱스가 가장 빠른 친구를 리턴
+	return tmpArr[Math.min(...result)];
+};
