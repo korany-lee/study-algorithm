05b6f39b493f374ee576d710cc55143d944a8456
dongoc <dongoc21hj@gmail.com>
2020-09-02 21:41:52 +0900

    200901 toy problem #1, #2

---
+++ codeSharing/toyProblem/02_nonrepeatCharacter/dongoc.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const firstNonRepeatedCharacter = function (string) {
+	let arr = string.split('');
+	let obj = arr.reduce((a, c) => {
+		if (a[c] === undefined) {
+			a[c] = 0;
+		} else {
+			a[c] = 1;
+		}
+		return a;
+	}, {});
+	for (let key in obj) {
+		if (obj[key] === 0) {
+			return key;
+		}
+	}
+};
