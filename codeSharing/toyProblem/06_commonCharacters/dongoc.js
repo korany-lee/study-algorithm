d65bf9da730c153f5ac72346801e57201b638600
dongoc <dongoc21hj@gmail.com>
2020-09-06 19:51:45 +0900

    dongoc: add dongoc's toy 06

---
+++ codeSharing/toyProblem/06_commonCharacters/dongoc.js
@@ -0,0 +1,31 @@
+const commonCharacters = function (string1, string2) {
+  let args = [...arguments].map(string => string.split(' ').join('').split(''));
+  let [first, ...rest] = args;
+  first = [...new Set(first)];
+  return first.filter(letter => rest.every(string => string.includes(letter))).join('')
+};
+
+// 재귀!로도 풀어보기
+
+/*
+const commonCharacters = function(string1, string2) {
+  let args = [...arguments].map(string => string.split(' ').join('').split(''));
+  let objs = args.map(arr => arr.reduce((a, c) => {
+    if (a[c] === undefined) a[c] = 1;
+    return a;
+  }, {}))
+  let commonCount = {};
+  objs.forEach(el => {
+    for (let key in el) {
+      if (!commonCount[key]) commonCount[key] = 1;
+      else commonCount[key]++;
+    }
+  })
+  for (let key in commonCount) {
+    if (commonCount[key] !== args.length) {
+      delete commonCount[key];
+    }
+  }
+  return Object.keys(commonCount).join('');
+};
+*/
