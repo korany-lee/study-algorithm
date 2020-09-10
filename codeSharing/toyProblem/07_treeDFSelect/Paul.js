2ed4e0561a2d914bf98e71a4d0864353ea9796b6
QuePark <gyuhapark11@gmail.com>
2020-09-10 19:05:05 +0900

    Set a test.js until #11 of toyProblem

--- codeSharing/toyProblem/07_treeDFSelect/Paul.js
+++ codeSharing/toyProblem/07_treeDFSelect/Paul.js
@@ -6,22 +6,22 @@
  */
 
 let Tree = function (value) {
-  this.value = value;
-  this.children = [];
+	this.value = value;
+	this.children = [];
 };
 
 Tree.prototype.DFSelect = function (filter, depth, visited) {
-  visited = visited || []; // 리커젼 할때 찾아내서 푸쉬하넥 없으면 empty array 로 시작
-  depth = depth || 0; // depth 는 리커젼 돌때마다 하나하나씩 늘어난다.
-  if (filter(this.value, depth)) {
-    visited.push(this.value); //visited 에다가 발견값을 푸쉬
-  }
-  let count = 0;
-  while (count < this.children.length) {
-    this.children[count].DFSelect(filter, depth + 1, visited); //뎁쓰 +1 주고 다음 뎁쓰 탐험
-    count += 1;
-  }
-  return visited;
+	visited = visited || []; // 리커젼 할때 찾아내서 푸쉬하넥 없으면 empty array 로 시작
+	depth = depth || 0; // depth 는 리커젼 돌때마다 하나하나씩 늘어난다.
+	if (filter(this.value, depth)) {
+		visited.push(this.value); //visited 에다가 발견값을 푸쉬
+	}
+	let count = 0;
+	while (count < this.children.length) {
+		this.children[count].DFSelect(filter, depth + 1, visited); //뎁쓰 +1 주고 다음 뎁쓰 탐험
+		count += 1;
+	}
+	return visited;
 };
 
 /*
@@ -33,47 +33,48 @@ Tree.prototype.DFSelect = function (filter, depth, visited) {
  * (Tree가 아닌 값이 들어올 경우, Tree 객체 형태로 변환 후 추가합니다.)
  */
 Tree.prototype.addChild = function (child) {
-  if (!child || !(child instanceof Tree)) {
-    child = new Tree(child);
-  }
+	if (!child || !(child instanceof Tree)) {
+		child = new Tree(child);
+	}
 
-  if (!this.isDescendant(child)) {
-    this.children.push(child);
-  } else {
-    throw new Error('That child is already a child of this tree');
-  }
-  // 편의를 위해 추가된 child node를 return합니다.
-  return child;
+	if (!this.isDescendant(child)) {
+		this.children.push(child);
+	} else {
+		throw new Error('That child is already a child of this tree');
+	}
+	// 편의를 위해 추가된 child node를 return합니다.
+	return child;
 };
 
 /*
  * 주어진 tree가 이미 해당 tree 혹은 sub tree의 child인지 확인합니다.
  */
 Tree.prototype.isDescendant = function (child) {
-  if (this.children.indexOf(child) !== -1) {
-    // `child`는 해당 트리와 연결된 하위 노드를 의미합니다.
-    return true;
-  } else {
-    for (let i = 0; i < this.children.length; i++) {
-      if (this.children[i].isDescendant(child)) {
-        return true;
-      }
-    }
-    return false;
-  }
+	if (this.children.indexOf(child) !== -1) {
+		// `child`는 해당 트리와 연결된 하위 노드를 의미합니다.
+		return true;
+	} else {
+		for (let i = 0; i < this.children.length; i++) {
+			if (this.children[i].isDescendant(child)) {
+				return true;
+			}
+		}
+		return false;
+	}
 };
 
 /*
  * child를 삭제합니다.
  */
 Tree.prototype.removeChild = function (child) {
-  let index = this.children.indexOf(child);
-  if (index !== -1) {
-    // child를 삭제합니다.
-    this.children.splice(index, 1);
-  } else {
-    throw new Error('That node is not an immediate child of this tree');
-  }
+	let index = this.children.indexOf(child);
+	if (index !== -1) {
+		// child를 삭제합니다.
+		this.children.splice(index, 1);
+	} else {
+		throw new Error('That node is not an immediate child of this tree');
+	}
 };
 
 // value 랑 depth 를 넣어줘야 한다.
+module.exports = Tree;
--- codeSharing/toyProblem/08_largestProductOfThree/dongoc.js
+++ codeSharing/toyProblem/08_largestProductOfThree/dongoc.js
@@ -1,10 +1,15 @@
 const largestProductOfThree = function (array) {
-  let sorted = array.slice().sort((a, b) => b - a);
-  let [neg2, neg1] = array.filter(num => num < 0).sort((a, b) => a - b);
-  return Math.max(...[sorted[0] * sorted[1] * sorted[2], sorted[0] * neg1 * neg2].filter(num => !Object.is(num, NaN)));
+	let sorted = array.slice().sort((a, b) => b - a);
+	let [neg2, neg1] = array.filter((num) => num < 0).sort((a, b) => a - b);
+	return Math.max(
+		...[sorted[0] * sorted[1] * sorted[2], sorted[0] * neg1 * neg2].filter(
+			(num) => !Object.is(num, NaN)
+		)
+	);
 };
 // 경우의 수
 // 1. 양수3 -> 가장 큰 양수 3개
 // 2. 음수2 양수 -> 가장 큰 양수 1개, 가장 작은 음수 2개
 // 3. 음수1 양수2 -> 가장 작은 양수 2개, 가장 큰 음수 1개 -> 1번과 같음 (어쩔 수 없는 경우이기 때문)
 // 4. 음수3 -> 가장 큰 음수 3개 -> 1번과 같음
+module.exports = largestProductOfThree;
